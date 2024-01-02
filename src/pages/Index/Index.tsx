f7f6ffd132bcd84ecab04e5a856b4c3e755f25b3
Manuel Atance <atance3@hotmail.com>
2024-01-02 14:36:04 +0100

    Added wave and onscroll effect

--- src/pages/Index/Index.tsx
+++ src/pages/Index/Index.tsx
@@ -1,10 +1,12 @@
 import { Hero } from "../../components/Hero/Hero";
+import { Wave } from "../../components/Wave/Wave";
 
 export function Index() {
 
   return (
     <main>
       <Hero />
+      <Wave />
     </main>
   )
 }
